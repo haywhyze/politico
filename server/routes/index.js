351611bf8a4bcc07e40278ffc61baf2f4a283f12
Yusuf Ayo <haywhyze@gmail.com>
2019-01-27 13:58:23 +0100

    feat(server): implement get all office
    - add the `export default router` statement to the bootom of the page.
    
    [delivers #163424201]

--- server/routes/index.js
+++ server/routes/index.js
@@ -26,5 +26,6 @@ router.delete('/parties/:id', validateID, PartyController.delete);
 
 // office routes
 router.post('/offices', isEmpty, validateType, validateOfficeName, OfficeContrller.create);
-export default router;
 router.get('/offices', OfficeContrller.getAll);
+
+export default router;
