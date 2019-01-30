9c15a414d4743bff7a7e0bf204d51a3f9026f980
Yusuf Ayo <haywhyze@gmail.com>
2019-01-30 18:09:30 +0100

    Update validateType.js
    - update error message to indicate accepted types in order not to confuse the user
    
    [delivers #163424172]

--- server/middlewares/validateType.js
+++ server/middlewares/validateType.js
@@ -4,7 +4,7 @@ const isValidType = (req, res, next) => {
   if (!acceptedType.includes(req.body.type)) {
     return res.status(400).send({
       status: 400,
-      error: 'Type not of accepted value',
+      error: 'Type not valid. Valid types are: federal, legislature, state and local',
     });
   }
   return next();
